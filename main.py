96b8cbe971a9e52ca69d7a3a4cbd211b71a3be05
Nathan Lawrence <lawrencen@missouri.edu>
2015-12-22 11:16:45 -0600

    More argument parsing.

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ from BeautifulSoup import BeautifulSoup
 
 #Define and parse command line arguments.
 argParser = argparse.ArgumentParser(description='Processes a 1-column CSV file filled with URLs into a folder of images from those pages.')
+argParser.add_argument('source')
+argParser.add_argument('-o', '--output')
+
+
+#If no source argument is present, return the help screen.
 argParser.print_help()
